b519e804de954c35a7fe8fd149236c8aebe6fd95
Daniel Brian <db9755949@gmail.com>
2024-01-24 18:26:32 +0300

    Code explanation

--- controllers/foodController.go
+++ controllers/foodController.go
@@ -19,74 +19,108 @@ import (
 // func(ctx *gin.Context) represents an anonymous function that actually handles the request
 // [ctx *gin.Context] represents the actual HTTP request and response
 
+// function that creates a food collection in the database
 var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
+
+// used to struct field validation
 var validate = validator.New()
 
 func GetFoods() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
-		foodId := c.Param("food_id")
-		var food models.Food
-
-		err := foodCollection.FindOne(ctx, bson.M{"food_id": foodId}).Decode(&food)
-		defer cancel()
-
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while fetching the food item"})
-		}
-		c.JSON(http.StatusOK, food)
 	}
 }
 
 func GetFood() gin.HandlerFunc {
-	return func(ctx *gin.Context) {
+	// function to handle the food item
+	return func(c *gin.Context) {
+		// Creates a context with a timeout of 100 seconds
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		// Retrieves the value of the "food_id" parameter from the request
+		foodId := c.Param("food_id")
+		// Create an instance of the "Food" struct from the "models" package
+		var food models.Food
 
+		// Mongodb's "FindOne" method to query for a document with a matching "food_id"
+		// in the "foodCollection". Decode the result into the "food" variable
+		err := foodCollection.FindOne(ctx, bson.M{"food_id": foodId}).Decode(&food)
+		// Ensures the sorrounding context is closed when the GetFood completes
+		defer cancel()
+
+		// Handles any errors that occurred during the MongoDB query,returns an internal server error incase
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while fetching the food item"})
+		}
+
+		// Error is nil so it  returns the fetched food item as a JSON response
+		// with a status of OK
+		c.JSON(http.StatusOK, food)
 	}
 }
 
 func CreateFood() gin.HandlerFunc {
+	// function that handles the creation of food request
 	return func(c *gin.Context) {
+		// creating a context with a time out of 100 seconds
 		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+		// creating the instances of the food and menu structs
 		var menu models.Menu
 		var food models.Food
 
+		// binding the Json data from the Http request body to the food variable
+		// and returning an error if data is not nil
 		if err := c.BindJSON(&food); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
 
+		// checking whether the data entered in the struct from the Http is in the right format
+		// the validate functions returns json error with status bad request
 		validationErr := validate.Struct(food)
 		if validationErr != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
 			return
 		}
 
-		err := menuCollection.FindOne(ctx,bson.M{"menu_id":food.Menu_id}).Decode(&menu)
+		// Querying the menu collection to find a menu based on food.menu_id
+		err := menuCollection.FindOne(ctx, bson.M{"menu_id": food.Menu_id}).Decode(&menu)
+		// cancelling the context after the menu querying
 		defer cancel()
 
-		if err != nil{
+		// Handling the error incase the querying is not successful
+		if err != nil {
 			msg := fmt.Sprint("menu was not found")
-			c.JSON(http.StatusInternalServerError,gin.H{"error":msg})
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
 			return
 		}
 
-		food.Created_at,_ = time.Parse(time.RFC3339,time.Now().Format(time.RFC3339))
-		food.Updated_at,_ = time.Parse(time.RFC3339,time.Now().Format(time.RFC3339))
+		// creating the timestamps when the food is created
+		// the parse function receives a layout and a value, time.RFC3339, time.Now().Format(time.RFC3339)
+		food.Created_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		food.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
 
+		// creating a new id for the food collection
+		// the hex function assigns a hexadecimal representation to the generated id
 		food.ID = primitive.NewObjectID()
 		food.Food_id = food.ID.Hex()
-		var num = toFixed(*food.Price,2)
+
+		// Rounding up the food price to two decimal places
+		var num = toFixed(*food.Price, 2)
 		food.Price = &num
 
-		result,insertErr := foodCollection.InsertOne(ctx,food)
+		// Inserting the food struct into the food collection
+		result, insertErr := foodCollection.InsertOne(ctx, food)
 		if insertErr != nil {
+			// Error handling incase the insertion has failed
 			msg := fmt.Sprintf("Food item was not created")
-			c.JSON(http.StatusInternalServerError,gin.H{"error":msg})
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
 			return
 		}
-		
+        // canceling the context after a insertion to the database
 		defer cancel()
-		c.JSON(http.StatusOK,result)
+
+		// returning the result of the insertion as the response
+		c.JSON(http.StatusOK, result)
 	}
 }
 
--- controllers/menuController.go
+++ controllers/menuController.go
@@ -16,22 +16,37 @@ import (
 // func(ctx *gin.Context) represents the actual request handler for the routes
 // [ctx *gin.Context] represents the actual parameters for the current HTTP request and response
 
+// opens a MongoDb collection specified by the name menu
 var menuCollection *mongo.Collection = database.OpenCollection(database.Client,"menu")
 
 func GetMenus() gin.HandlerFunc{
+	// Handler function for getting the menu items
 	return func(c *gin.Context) {
+		// creating a context with a timeout of 100 seconds
 		var ctx,cancel = context.WithTimeout(context.Background(),100*time.Second)
+		
+		// Finding all the documents in the menuCollection.
+		// bson.M{} is an empty filter indicating that all documents should be retrieved
 		result,err :=  menuCollection.Find(context.TODO(),bson.M{})
+
+		// Ensures that the sorrounding context is closed when the GetMenus completes
 		defer cancel()
 
+		// Checks if an error occured during the mongodb operation. If there's the error returns a json
+		// and function exits
 		if err != nil {
 			c.JSON(http.StatusInternalServerError,gin.H{"error":"error occured while listing the menu item"})
 		}
 
+		// Declaring a slice to store the retrieved menu items
 		var allMenus []bson.M
+
+		// Extract all documents,from the mongodb result into the  "allmenu" slice
 		if err = result.All(ctx,&allMenus); err != nil {
 			log.Fatal(err)
 		}
+
+		// If everything is successful, the retrieved menu items are returned as a JSON response
         c.JSON(http.StatusOK,allMenus)
 	}
 }
